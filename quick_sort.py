5404a9dce0a522d5658ec1a97bdc5229f33eb0c7
qcai2002 <qcai2002@gmail.com>
2020-04-30 21:24:14 -0700

    quick sort version 1

--- quick_sort.py
+++ quick_sort.py
@@ -1,12 +1,12 @@
     def partition(nums, left, right):
-        n = right - left + 1 
-        print("\narray", nums[left:right+1], "left", nums[left], "right", nums[right])
+        n = right - left 
+        print("\narray", nums[left:right], "left", nums[left], "right", nums[right-1])
         if (n <= 1):
             return 0
         original_left = left
         original_right = right     
-        pivot_idx = right
-        right -= 1
+        pivot_idx = right - 1
+        right = pivot_idx - 1
         pivot = nums[pivot_idx]
         print("pivot", pivot, "length of array", n)
 
@@ -27,18 +27,18 @@
             pivot_idx = left
             print("switch, left, pivot_idx", left, pivot_idx, "values", nums[left], nums[pivot_idx])
             
-        print("after partition, ", nums[original_left:original_right+1], "left", left, "right", right)
+        print("after partition, ", nums[original_left:original_right], "left", left, "right", right)
               
         #divide into two subarrays
         if (pivot_idx > original_left): # pivot_idx might be 0
             print("left subarray ", nums[original_left:pivot_idx])
-            partition(nums, original_left, pivot_idx-1) 
+            partition(nums, original_left, pivot_idx) 
             
-        if (pivot_idx + 1 < original_right+1):
-            print("right subarray", nums[pivot_idx+1:original_right+1])
+        if (pivot_idx + 1 < original_right):
+            print("right subarray", nums[pivot_idx+1:original_right])
             partition(nums, pivot_idx+1, original_right)
             
         return
         
     def quick_sort(arr):
-        return partition(arr, 0, len(arr) - 1)
+        return partition(arr, 0, len(arr)) 
