2e9e7cf2b101d897a97d4ef57da5eae62c903113
qcai2002 <qcai2002@gmail.com>
2020-05-03 22:33:55 -0700

    shortest path v2

--- shortest_path.py
+++ shortest_path.py
@@ -1,12 +1,17 @@
 def shortest_path(dist, n, orig):
-    result = {i: 1000 for i in range(1,6)}
+    BIG_VALUE = 1000
+    result = {i: BIG_VALUE for i in range(1,n+1)}
+    result[orig] = 0
     visited = {}
     
     current = orig
     visited[orig] = 0
     while (len(visited) <= n):
         print("visited", visited, "result", result)
-        direct_path = dist[current]
+        if current in dist:
+            direct_path = dist[current]
+        else:
+            direct_path = {}
         if (len(visited) == 1):
             base = 0
         else:
@@ -16,7 +21,7 @@ def shortest_path(dist, n, orig):
                 result[k] = base + direct_path[k]
         
         #find the next node to process
-        min_value = 1000
+        min_value = BIG_VALUE
         min_idx = -1
         for k in result:
             if k not in visited:
@@ -24,13 +29,17 @@ def shortest_path(dist, n, orig):
                     min_value = result[k]
                     min_idx = k
         if (min_idx == -1):
-            return result
+            max_dist = max(result.values())
+            max_dist = max_dist if max_dist != BIG_VALUE else -1
+            return max_dist, result            
         else:
             current = min_idx
             visited[current] = 0 
-    return result
+    max_dist = max(result.values())
+    max_dist = max_dist if max_dist != BIG_VALUE else -1
+    return max_dist, result
 
-Cities = ["Atlanta", "Boston", "Chicago", "Denver", "El Paso"]
+cities = ["Atlanta", "Boston", "Chicago", "Denver", "El Paso"]
 times = [[1,2,100],[1,4,160], [2,4,180],[2,3,120], [3,5,80], [4,3,40],[4,5,140], [5,2,100]]
 
 def conv_to_ditn(times):
@@ -46,6 +55,12 @@ def conv_to_ditn(times):
             d[a[0]] = d0
     return d
 
-for orig in range(1,6):
+n = len(cities)
+for orig in range(1,n+1):
     print("\n")
-    shortest_path(conv_to_ditn(times), 5, orig)
+    print(shortest_path(conv_to_ditn(times), n, orig))
+
+times = [[2,1,1],[2,3,1],[3,4,1]]
+n = 4
+orig = 2
+shortest_path(conv_to_ditn(times), n, orig)
